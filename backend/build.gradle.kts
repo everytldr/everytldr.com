1f789009e2deb09520f972e78fd352597edf0503
jeheecheon <jeheecheon@gmail.com>
2026-04-27 18:43:23 +0900

    feat: add flyway starter and initial db migration schema

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-restclient")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
-    implementation("org.flywaydb:flyway-core")
+    implementation("org.springframework.boot:spring-boot-starter-flyway")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.mysql:mysql-connector-j")
