0282086de77c254e17f0b2d90f18307d64d78972
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2026-05-04 16:38:00 +0900

    refactor: generalize RSS source as article source

--- backend/src/main/java/org/tldrtimes/common/domain/source/ArticleSourceRepository.java
+++ backend/src/main/java/org/tldrtimes/common/domain/source/ArticleSourceRepository.java
@@ -3,6 +3,6 @@ package org.tldrtimes.common.domain.source;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RssSourceRepository extends JpaRepository<RssSource, Long> {
-  List<RssSource> findAllByIsActiveTrue();
+public interface ArticleSourceRepository extends JpaRepository<ArticleSource, Long> {
+  List<ArticleSource> findAllByIsActiveTrue();
 }
