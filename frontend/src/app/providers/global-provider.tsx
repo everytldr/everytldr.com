be4ed09948f361fcf91ce6ea943cdb228980d50c
jeheecheon <jeheecheon@gmail.com>
2026-05-06 10:48:16 +0900

    refactor: move theme provider to shared module

--- frontend/src/app/providers/global-provider.tsx
+++ frontend/src/app/providers/global-provider.tsx
@@ -1,9 +1,9 @@
+import { ThemeProvider } from "@/shared/theme";
 import { NextIntlClientProvider } from "next-intl";
 import { cookies } from "next/headers";
 import { type PropsWithChildren } from "react";
 import { SyncedStorageProvider } from "synced-storage/react";
 import { QueryProvider } from "./query-provider";
-import { ThemeProvider } from "./theme-provider";
 
 export async function GlobalProvider({ children }: PropsWithChildren) {
   const cookieStore = await cookies();
