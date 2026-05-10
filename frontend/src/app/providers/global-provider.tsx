4fbe32f50b96163bd0c87317d942a869ddf25154
jeheecheon <jeheecheon@gmail.com>
2026-05-11 03:05:25 +0900

    chore: restore synced-storage for planned future use

--- frontend/src/app/providers/global-provider.tsx
+++ frontend/src/app/providers/global-provider.tsx
@@ -1,14 +1,20 @@
 import { ThemeProvider } from "@/shared/theme";
 import { NextIntlClientProvider } from "next-intl";
+import { cookies } from "next/headers";
 import { type PropsWithChildren } from "react";
+import { SyncedStorageProvider } from "synced-storage/react";
 import { QueryProvider } from "./query-provider";
 
-export function GlobalProvider({ children }: PropsWithChildren) {
+export async function GlobalProvider({ children }: PropsWithChildren) {
+  const cookieStore = await cookies();
+
   return (
-    <ThemeProvider>
-      <NextIntlClientProvider>
-        <QueryProvider>{children}</QueryProvider>
-      </NextIntlClientProvider>
-    </ThemeProvider>
+    <SyncedStorageProvider ssrCookies={cookieStore.getAll()}>
+      <ThemeProvider>
+        <NextIntlClientProvider>
+          <QueryProvider>{children}</QueryProvider>
+        </NextIntlClientProvider>
+      </ThemeProvider>
+    </SyncedStorageProvider>
   );
 }
