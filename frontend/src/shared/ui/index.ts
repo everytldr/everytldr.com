bbf3707801984c90077aa1715e733638137f3fef
jeheecheon <jeheecheon@gmail.com>
2026-05-13 19:53:07 +0900

    feat: add ScrollableRow with overflow arrow navigation

--- frontend/src/shared/ui/index.ts
+++ frontend/src/shared/ui/index.ts
@@ -9,5 +9,6 @@ export { default as Logo } from "./logo.svg";
 export { Modal } from "./modal";
 export { ResponsiveDialog } from "./responsive-dialog";
 export { ResponsiveSelector, type SelectorOption } from "./responsive-selector";
+export { ScrollableRow } from "./scrollable-row";
 export { Tabs, TabsContent, TabsList, TabsTrigger } from "./tabs";
 export { Translation } from "./translation";
