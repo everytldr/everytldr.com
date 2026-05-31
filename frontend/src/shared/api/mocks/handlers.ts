d54d1d2cb9b6d0c8f827c8f54ef25c1bba2c4786
jeheecheon <jeheecheon@gmail.com>
2026-06-01 01:29:37 +0900

    feat: add article comment and like mock handlers

--- frontend/src/shared/api/mocks/handlers.ts
+++ frontend/src/shared/api/mocks/handlers.ts
@@ -1,7 +1,18 @@
 import { http } from "msw";
-import { getArticle, listArticles } from "./fetchers/article";
+import {
+  getArticle,
+  getMyArticleLike,
+  likeArticle,
+  listArticleComments,
+  listArticles,
+  unlikeArticle,
+} from "./fetchers/article";
 
 export const handlers = [
   http.get("*/api/articles", listArticles),
+  http.get("*/api/articles/:articleId/comments", listArticleComments),
+  http.get("*/api/articles/:articleId/likes/me", getMyArticleLike),
+  http.put("*/api/articles/:articleId/likes/me", likeArticle),
+  http.delete("*/api/articles/:articleId/likes/me", unlikeArticle),
   http.get("*/api/articles/:id", getArticle),
 ];
