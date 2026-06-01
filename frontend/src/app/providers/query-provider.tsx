821534acd34f74bd066800fd64bc22e6f7237dd6
jeheecheon <jeheecheon@gmail.com>
2026-06-01 15:13:59 +0900

    chore: add frontend support dependencies

--- frontend/src/app/providers/query-provider.tsx
+++ frontend/src/app/providers/query-provider.tsx
@@ -2,9 +2,15 @@
 
 import { getQueryClient } from "@/shared/api";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { type PropsWithChildren } from "react";
 
 export function QueryProvider({ children }: PropsWithChildren) {
   const client = getQueryClient();
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
