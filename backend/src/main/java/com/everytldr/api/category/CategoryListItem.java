7b21bc07946515450e1214399030fd57c0d4a1ef
jeheecheon <jeheecheon@gmail.com>
2026-06-14 04:09:25 +0900

    chore: mark api response fields as required

--- backend/src/main/java/com/everytldr/api/category/CategoryListItem.java
+++ backend/src/main/java/com/everytldr/api/category/CategoryListItem.java
@@ -1,8 +1,10 @@
 package com.everytldr.api.category;
 
 import com.everytldr.common.domain.category.Category;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 
-public record CategoryListItem(String slug) {
+public record CategoryListItem(@Schema(requiredMode = RequiredMode.REQUIRED) String slug) {
   static CategoryListItem from(Category category) {
     return new CategoryListItem(category.getSlug());
   }
