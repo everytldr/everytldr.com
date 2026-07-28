5ea1f8ecf871be95bebfe2f3377e26330fa476f4
jeheecheon <jeheecheon@gmail.com>
2026-07-28 15:51:33 +0900

    fix: point steiger at the fsd root

--- frontend/steiger.config.ts
+++ frontend/steiger.config.ts
@@ -1,4 +1,12 @@
 import fsd from "@feature-sliced/steiger-plugin";
 import { defineConfig } from "steiger";
 
-export default defineConfig([...fsd.configs.recommended]);
+export default defineConfig([
+  ...fsd.configs.recommended,
+  {
+    files: ["./src/widgets/**"],
+    rules: {
+      "fsd/insignificant-slice": "warn",
+    },
+  },
+]);
